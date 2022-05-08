ece131c5973897c34d83fdba7af2d9ecab51ee76
JustaSqu1d <overenchanted.gaming@gmail.com>
2022-05-07 21:53:46 -0700

    Update wr.py

--- cogs/wr.py
+++ cogs/wr.py
@@ -34,7 +34,7 @@ class Wrall(Cog):
             us = entry["user"].split("|")
             score = re_format(int(us[1]))
             user = await self.bot.fetch_user(us[0])
-            return f"\n{user.name} | [{score}](<{entry['link']}>)\n"
+            return f"\n[{user.name} | {score}]({entry['link']})\n"
 
         async def ships_f(embed):
             embed.add_field(name='Ship',
