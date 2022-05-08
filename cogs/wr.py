431ee60c6584bfe5c032ec643547d88694494785
JustaSqu1d <overenchanted.gaming@gmail.com>
2022-05-08 10:31:19 -0700

    Update wr.py

--- cogs/wr.py
+++ cogs/wr.py
@@ -34,7 +34,7 @@ class Wrall(Cog):
             us = entry["user"].split("|")
             score = re_format(int(us[1]))
             user = await self.bot.fetch_user(us[0])
-            return f"<{score}>\n<{user.name}>"
+            return f"{score}\n{user.name}"
 
         async def ships_f(embed):
             embed.add_field(name='Ship',
@@ -70,22 +70,22 @@ class Wrall(Cog):
 
             #coros = [ships_f(embed),ffa(embed),tdm2(embed)]
 
-            header = ["# Ship", "FFA", "2TDM"]
+            header = ["Ship", "FFA", "2TDM"]
 
             body = []
 
             for ship in ships:
                 
                 entry = []
-                entry.append(f"<{ship}>")
+                entry.append(f"{ship}")
                 if db2[ship]["FFA"]["High Score"]["1"]["user"] != 0:
                     entry.append(await lb_format(ship, db2, 'FFA'))
                 else:
-                    entry.append("<->")
+                    entry.append("-")
                 if db2[ship]["2 Teams"]["High Score"]["1"]["user"] != 0:
                     entry.append(await lb_format(ship, db2, '2 Teams'))
                 else:
-                    entry.append("<->")
+                    entry.append("-")
                 body.append(entry)
             
             #await gather(*coros)
